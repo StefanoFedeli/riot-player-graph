9ac62b18fe80a9bee7034f4f654b0bcfbb6d7562
matteo <mimmi.ferrini@gmail.com>
2020-10-20 09:55:35 +0200

    big mmmmmmmh

--- src/main/scala/producer/playerDataProducerFactory.scala
+++ src/main/scala/producer/playerDataProducerFactory.scala
@@ -3,12 +3,11 @@ package producer
 import scala.collection.mutable.ListBuffer
 import producer.playerDataProducer
 
-object playerDataProducerFactory(val playerList: List[String]){
+object playerDataProducerFactory(val API_KEY: String, val playerList: List[String]){
   def buildRetrievers(): List[playerDataProducer] = {
     var retrieversFactory = new ListBuffer[String]()
     for summonerId: String in playerList:
-    retrieversFactory += new playerDataProducer(summonerId)
+      retrieversFactory += new playerDataProducer(API_KEY, summonerId)
     return retrieversFactory
   }:
-
 }
